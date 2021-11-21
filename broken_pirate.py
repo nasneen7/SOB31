15804b16b72c899488468ac99a9ebd012be53c85
nasneen7 <nm1442@live.mdx.ac.uk>
2021-11-21 13:07:57 +0400

    all required changes done to broken_pirate

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,6 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?")
+#added double qoutes before the paranthesis
+if greeting in ["Arrr!"]:  #replaces round bracket with a square bracket
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else:#elif changed to else
+        print("Greetings, hater of pirates!")  #indentation done
