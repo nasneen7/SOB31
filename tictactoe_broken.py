4498f9cdcebb1aaa32d8c585f636e83b4eec8fab
nasneen7 <nm1442@live.mdx.ac.uk>
2021-11-21 13:33:30 +0400

    all required changes done to tictactoe_broken

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -1,7 +1,7 @@
-#tictactoe game.
-
+# tictactoe game.
 # Find the 4 errors in the code and fix them,
 # so the game works as expected.
+
 
 def draw_line(width, edge, filling):
     print(filling.join([edge] * (width + 1)))
@@ -13,11 +13,11 @@ def display_winner(player):
     else:
         print("Player " + str(player) + " wins!")
 
+
 def check_row_winner(row):
-    """
-    Return the player number that wins for that row.
-    If there is no winner, return 0.
-    """
+    """Return the player number that wins for that row.
+       If there is no winner, return 0."""
+
     if row[0] == row[1] and row[1] == row[2]:
         return row[0]
     return 0
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index row
     column: 0-index column
     """
-    game[row][column+1] = player
+    game[row][column] = player  # removed +1
     return game
 
 def check_space_empty(game, row, column):
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: #'=' changed to '=='
         return 2
     else:
         return 1
@@ -98,10 +98,10 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available:  #colon added
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game,row,column)  #added column argument to check_empty_space_empty() function
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
